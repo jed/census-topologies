9596ea6ed27015a34d8839693f00edec93182077
Jed Schmidt <where@jed.is>
2016-11-16 01:05:59 -0500

    rejigger

--- build.sh
+++ build.sh
@@ -3,22 +3,18 @@
 mkdir -p types /tmp/census-topologies
 cd /tmp/census-topologies
 
-curl -s 'ftp://ftp2.census.gov/geo/tiger/GENZ2015/shp/' |
-  grep '_500k.zip$' |
-  sed 's|.* |http://www2.census.gov/geo/tiger/GENZ2015/shp/|' |
-  while read -r URL; do [ -f ${URL##*/} ] || curl -O $URL; done
+curl 'ftp://ftp2.census.gov/geo/tiger/GENZ2015/shp/' \
+| grep '_500k.zip$' \
+| sed 's|.* |http://www2.census.gov/geo/tiger/GENZ2015/shp/|' \
+| while read -r URL; do [ -f ${URL##*/} ] || curl -O $URL; done
 
-unzip -o '*.zip' '*.shp' '*.dbf'
-rm -f *.geojson
+unzip -o '*.zip'
 
-for FILE in $(ls *.shp); do
-  TYPE=$(echo $FILE | cut -d'_' -f 4)
-  MAP='(d.id = d.properties.GEOID || d.properties.GEOID10, delete d.properties, d)'
+TYPES=$(ls *.shp | sed 's/cb_2015_.._\(.*\)_500k.shp$/\1/' | sort | uniq)
 
-  shp2json -n $FILE | ndjson-map "$MAP" >> ../build/$TYPE.geojson
-done
-
-for FILE in $(ls *.geojson); do
-  TYPE=$(basename $FILE .geojson)
-  geo2topo -q 1e5 -n $TYPE.geojson > ../types/$TYPE.json
+for TYPE in $TYPES; do
+  for FILE in $(ls cb_2015_*_${TYPE}_500k.shp); do shp2json -n $FILE; done \
+  | ndjson-map '(d.id = d.properties.GEOID || d.properties.GEOID10, delete d.properties, d)' \
+  | geo2topo -n -q 1e5 $TYPE=- \
+  > $OLDPWD/types/$TYPE.json
 done
